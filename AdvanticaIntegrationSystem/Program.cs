302b0314a542dde4b0682bb8e95f20c628fc25f7
Эмин Назаров <nazarov.emin@outlook.com>
2023-07-08 16:56:29 +0300

    Add new project to represent integration system. Change a set of grpc classes to server/client only

---
+++ AdvanticaIntegrationSystem/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace AdvanticaIntegrationSystem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
